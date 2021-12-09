30871696d217e8379b7e43de53d962d6f796e1a3
anamikagupta17 <40865637+anamikagupta17@users.noreply.github.com>
2021-12-10 01:00:10 +0530

    change password and update Profile

---
+++ P21AdminChangePassword/enroll/views.py
@@ -0,0 +1,87 @@
+from django.shortcuts import render
+from .froms import SignUpForm
+from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm,PasswordChangeForm,SetPasswordForm
+from django.contrib.auth import authenticate,login,logout, update_session_auth_hash
+from django.http import HttpResponseRedirect
+# Create your views here.
+
+def signup(req):
+    if req.method=='POST':
+        fm=SignUpForm(req.POST)
+        if fm.is_valid():
+            fm.save()
+            messages.success(req,'User Added Successfully')
+            fm=SignUpForm()
+            
+    else:        
+      fm=SignUpForm()
+      
+    return render (req,'signup.html',{'fm':fm})
+  
+  
+def userLogin(request):
+  if not request.user.is_authenticated:
+    if request.method == 'POST':
+      fm=AuthenticationForm(request=request,data=request.POST)
+      if fm.is_valid():
+            uname=fm.cleaned_data['username']
+            upass=fm.cleaned_data['password']
+            user=authenticate(username=uname,password=upass) # authenticate from db means match username and password from db
+            print(user)
+            if user is not None:
+              login(request, user)
+              messages.success(request,'Login Successfully')
+              return HttpResponseRedirect('/profile/')
+    else:
+      fm=AuthenticationForm()
+    return render (request,'login.html',{'form':fm})
+  else:
+    return HttpResponseRedirect('/profile/')
+
+
+def profile(req):
+  if req.user.is_authenticated: #check user is authenticated or not means loggedin user 
+     return render(req,'profile.html',{'name':req.user})
+  else :
+     return HttpResponseRedirect('/login/')
+
+def userLogout(req):
+  logout(req)
+  return HttpResponseRedirect('/login/')
+
+def changepas(req):
+  if req.user.is_authenticated: 
+    if req.method == 'POST':
+      fm=PasswordChangeForm(user=req.user,data=req.POST)
+      if fm.is_valid():
+        fm.save()
+        update_session_auth_hash(req,fm.user) # otherwise it will logout automatically because seesion expired.this will keep session update or active 
+        messages.success(req,'Password Changed Successsfully!')
+        return HttpResponseRedirect('/profile/')
+    else:
+      fm=PasswordChangeForm(user=req.user)
+    return render(req,'changepas.html',{'form':fm})
+  else :
+     return HttpResponseRedirect('/login/')
+   
+   
+def newchangepas(req):
+  if req.user.is_authenticated: 
+    if req.method == 'POST':
+      fm=SetPasswordForm(user=req.user,data=req.POST)
+      if fm.is_valid():
+        fm.save()
+        update_session_auth_hash(req,fm.user) # otherwise it will logout automatically because seesion expired.this will keep session update or active 
+        messages.success(req,'Password Changed Successsfully!')
+        return HttpResponseRedirect('/profile/')
+    else:
+      fm=SetPasswordForm(user=req.user)
+    return render(req,'newchangepas.html',{'form':fm})
+  else :
+     return HttpResponseRedirect('/login/')
+   
+
+  
+    
+    
---
+++ P22AdminProfile/enroll/froms.py
@@ -0,0 +1,17 @@
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm
+from django import forms 
+class SignUpForm(UserCreationForm):
+    password2=forms.CharField(label='Confirm Password(again)',widget=forms.PasswordInput)
+    class Meta:
+        model=User
+        fields=['username','first_name','last_name','email']  # we are not adding password fields because that is part of UserCreationForm that wil come automatically but we can validate separatly
+        labels={'email':'Email'}
+        
+        
+class EditUserProfileForm(UserChangeForm)  :
+    password=None     
+    class Meta:
+        model=User
+        fields=['username','first_name','last_name','email','date_joined','last_login','is_active','is_staff'] 
+        labels={'email':'Email','first_name':'First Name','date_joined':'Joining Date'}
